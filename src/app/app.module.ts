c613f8abcdeb14ed8c5e587999cad15ebc50dd02
Tracy Lee <tracy@wingman.events>
2017-02-14 13:39:15 -0800

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,12 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { CameraComponent } from './camera/camera.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CameraComponent
   ],
   imports: [
     BrowserModule,
